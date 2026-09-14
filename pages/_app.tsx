2eca2eebdf4a8d19fb971b9f1b44e0e2d0249161
Shawn <kakhkhorov97@gmail.com>
2026-09-14 16:14:23 +0900

    feat: MUI costume integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { useState } from "react";
+import { Light } from "@mui/icons-material";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // @ts-ignore
+  const [theme, setTheme] = useState(createTheme(Light));
+
+  // Socket.io, Redux, MUI...
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
